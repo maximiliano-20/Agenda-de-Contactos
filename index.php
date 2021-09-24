67f58804e9d6da0e756dfdc78bc9482ac71687ce
maximiliano-20 <estigarribiamaximiliano73@gmail.com>
2021-09-24 10:09:51 -0300

    Index Finalizado

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<title>Agenda de Contactos</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
 	<link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
@@ -18,15 +19,11 @@
 		</div>
 
 		<ul class="menu-link">
-			<li id="cantidad-contactos">Contactos</li>
+			<li id="cantidad-contactos"></li>
 			
 		</ul>
 		
 	</nav>
-
-	<div class="boton-menu">
-		<i class="fas fa-bars"></i>
-	</div>
 	
 </header>
 
@@ -40,17 +37,17 @@
 			
 			<div class="input-caja">
 				<label>Nombre</label>
-				<input type="text" name="nombre" id="nombre" placeholder="Ingrese un Nombre">
+				<input type="text" name="nombre" id="nombre" >
 			</div>
 
 			<div class="input-caja">
 				<label>Apellido</label>
-				<input type="text" name="apellido" id="apellido" placeholder="Ingrese un apellido">
+				<input type="text" name="apellido" id="apellido" >
 			</div>
 
 			<div class="input-caja">
 				<label>Telefono</label>
-				<input type="text" name="telefono" id="telefono" placeholder="Ingrese un telefono">
+				<input type="text" name="telefono" id="telefono" >
 			</div>
 
 		     <div class="boton-caja"> 
@@ -87,5 +84,6 @@
 
 	
 <script type="text/javascript" src="js/app.js"></script>
+<script type="text/javascript" src="sweet-alert/sweetalert2@9.js"></script>
 </body>
 </html>
